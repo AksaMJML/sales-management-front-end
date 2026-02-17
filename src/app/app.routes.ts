a18cf57a7f7fa6b808a4769f93409208f5f4561c
AksaMJML <akaks0186@gmail.com>
2026-02-17 22:17:20 +0530

    Refactor routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,62 @@
 import { Routes } from '@angular/router';
+import { Login } from './page/login/login';
+import { Dashboard } from './page/dashboard/dashboard';
+import { Customer } from './page/dashboard/customer/customer';
+import { Item } from './page/dashboard/item/item';
+import { Order } from './page/dashboard/order/order';
+import { AddCustomer } from './page/dashboard/customer/add-customer/add-customer';
+import { ViewAllCustomer } from './page/dashboard/customer/view-all-customer/view-all-customer';
+import { AddItem } from './page/dashboard/item/add-item/add-item';
+import { ViewAllItem } from './page/dashboard/item/view-all-item/view-all-item';
+import { ViewAllOrders } from './page/dashboard/order/view-all-orders/view-all-orders';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'logn',
+        component: Login
+    },
+    {
+        path: 'dashboard',
+        component : Dashboard,
+        children: [
+            {
+                path : 'cusatomer',
+                component : Customer,
+                children : [
+                    {
+                        path : 'add',
+                        component : AddCustomer
+                    },
+                    {
+                        path : 'view-all',
+                        component : ViewAllCustomer
+                    }
+                ]
+            },
+            {
+                path : 'item',
+                component : Item,
+                children : [
+                    {
+                        path : 'add',
+                        component : AddItem
+                    },
+                    {
+                        path : 'view-all',
+                        component : ViewAllItem
+                    }
+                ]
+            },
+            {
+                path : 'orer',
+                component : Order,
+                children : [
+                    {
+                        path : 'view-all',
+                        component : ViewAllOrders
+                    }
+                ]
+            }
+        ]
+    }
+];
